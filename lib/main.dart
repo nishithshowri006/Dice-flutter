2bc68978a53d8ad5d14a024204973f3d34d4aa3d
Nishith Showri <58651995+nishithshowri006@users.noreply.github.com>
2020-07-06 01:18:35 +0530

    Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.black,
         appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          title: Center(
+              child: Text(
+            'Dice',
+            style: TextStyle(fontSize: 20.0),
+          )),
+          backgroundColor: Colors.blueGrey[900],
         ),
         body: DicePage(),
       ),
@@ -15,9 +21,52 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+  void buttonClick() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Center(
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: FlatButton(
+                  highlightColor: Colors.white,
+                  padding: EdgeInsets.all(12.0),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0)),
+                  onPressed: () {
+                    buttonClick();
+                  },
+                  child: Image.asset("images/dice$leftDiceNumber.png")),
+            ),
+            Expanded(
+              child: FlatButton(
+                  padding: EdgeInsets.all(12.0),
+                  highlightColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0)),
+                  onPressed: () {
+                    buttonClick();
+                  },
+                  child: Image.asset("images/dice$rightDiceNumber.png")),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
